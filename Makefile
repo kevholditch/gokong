aaaf487082751895beb3cbcb0c27e660ddf7e426
Kevin Holditch <kevholditch@hotmail.com>
2017-11-06 20:26:17 +0000

    postgres container starting up

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 TEST?=$$(go list ./... |grep -v 'vendor')
 GOFMT_FILES?=$$(find . -name '*.go' |grep -v vendor)
 
-default: build test testacc
+default: build test
 
 build: fmtcheck errcheck vet
 	go install
@@ -12,9 +12,6 @@ test: fmtcheck
 	go test -i $(TEST) || exit 1
 	echo $(TEST) | \
 		xargs -t -n4 go test $(TESTARGS) -timeout=30s -parallel=4
-
-testacc: fmtcheck
-	FORM3_ACC=1 go test $(TEST) -v $(TESTARGS) -timeout 120m
 
 vet:
 	@echo "go vet ."
--- main_test.go
+++ main_test.go
@@ -1,80 +1,51 @@
-package go_sync
+package konggo
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"gopkg.in/ory-am/dockertest.v3"
 	"log"
-	"net/http"
 	"os"
 	"testing"
 )
 
-func TestAcc_sync_certificate_is_successful(t *testing.T) {
-	testPreCheck(t)
-
-}
+var db *sql.DB
 
 func TestMain(m *testing.M) {
-	log.SetOutput(os.Stdout)
+	var db *sql.DB
+	var err error
 	pool, err := dockertest.NewPool("")
 	if err != nil {
 		log.Fatalf("Could not connect to docker: %s", err)
 	}
 
-	options := &dockertest.RunOptions{
-		Repository: "consul",
-		Tag:        "0.8.5",
-	}
-
-	consulResource, err := pool.RunWithOptions(options)
+	resource, err := pool.Run("postgres", "9.6", []string{"POSTGRES_PASSWORD=kong", "POSTGRES_DB=kong"})
 	if err != nil {
-		log.Fatalf("Could not start consul: %s", err)
+		log.Fatalf("Could not start resource: %s", err)
 	}
 
-	if err := pool.Retry(func() error {
+	if err = pool.Retry(func() error {
 		var err error
-		resp, err := http.Get(fmt.Sprintf("http://localhost:%v/v1/catalog/services", consulResource.GetPort("8500/tcp")))
-		if err != nil || resp.StatusCode >= 400 {
+		db, err = sql.Open("postgres", fmt.Sprintf("postgres://postgres:kong@localhost:%s/kong?sslmode=disable", resource.GetPort("5432/tcp")))
+		if err != nil {
 			return err
 		}
-
-		log.Printf("Consul up: %v", resp.StatusCode)
-		return nil
+		return db.Ping()
 	}); err != nil {
-		log.Fatalf("Could not connect to consul: %s", err)
-	}
-
-	vaultResource, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Repository: "vault",
-		Tag:        "0.8.3",
-		Links:      []string{consulResource.Container.Name},
-		Env:        []string{"VAULT_DEV_ROOT_TOKEN_ID=C4B4BDE9-D318-421C-BF65-9AE4C3DA169B"},
-		Cmd:        []string{"-cap-add=IPC_LOCK"},
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := pool.Retry(func() error {
-		var err error
-		resp, err := http.Get(fmt.Sprintf("http://localhost:%v/v1/sys/health", vaultResource.GetPort("8200/tcp")))
-		if err != nil || resp.StatusCode >= 400 {
-			return err
-		}
-
-		log.Printf("Vault up: %v", resp.StatusCode)
-		return nil
-	}); err != nil {
-		log.Fatalf("Could not connect to vault: %s", err)
+		log.Fatalf("Could not connect to docker: %s", err)
 	}
 
 	code := m.Run()
 
-	// You can't defer this because os.Exit doesn't care for defer
-	if err := pool.Purge(consulResource); err != nil {
-		log.Fatalf("Could not purge consulResource: %s", err)
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge containers: %s", err)
 	}
 
 	os.Exit(code)
+}
+
+func TestSomething(t *testing.T) {
+	db.Query("SELECT 1")
+
 }
